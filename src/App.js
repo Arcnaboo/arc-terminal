e2fd3efa03ecf95acb92f66613bc037c1d444828
Arcnaboo <aakgur@gmail.com>
2025-07-16 02:00:14 +0300

    fix app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Terminal from "./components/Terminal";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <Terminal />
